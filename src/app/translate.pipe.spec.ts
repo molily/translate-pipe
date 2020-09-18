608c4fcef39c72cc9a9020e7733f891f13cf589d
Mathias Schaefer (molily) <molily@mailbox.org>
2020-09-19 00:03:25 +0200

    TranslatePipe spec

--- src/app/translate.pipe.spec.ts
+++ src/app/translate.pipe.spec.ts
@@ -5,7 +5,9 @@ import {
   TestBed,
   tick,
 } from '@angular/core/testing';
-import { Observable, of } from 'rxjs';
+import { Observable, of, VirtualTimeScheduler } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 import { TranslatePipe } from './translate.pipe';
 import { TranslateService, Translations } from './translate.service';
 
@@ -39,7 +41,6 @@ describe('TranslatePipe with HostComponent', () => {
     });
 
     translateService = TestBed.inject(TranslateService);
-    spyOn(translateService, 'get').and.callThrough();
 
     fixture = TestBed.createComponent(HostComponent);
     fixture.detectChanges();
@@ -60,9 +61,7 @@ describe('TranslatePipe with HostComponent', () => {
   });
 
   it('updates on translation change', () => {
-    (translateService.get as jasmine.Spy).and.callFake((key: string) =>
-      of(`[New translation for ${key}]`)
-    );
+    translateService.get = (key: string) => of(`[New translation for ${key}]`);
     translateService.onTranslationChange.emit({});
     fixture.detectChanges();
     expect(fixture.debugElement.nativeElement.textContent).toBe(
@@ -71,21 +70,93 @@ describe('TranslatePipe with HostComponent', () => {
   });
 });
 
-describe('TranslatePipe bare', () => {
+class FakeChangeDetectorRef implements Pick<ChangeDetectorRef, 'markForCheck'> {
+  markForCheck(): void {}
+}
+
+fdescribe('TranslatePipe bare', () => {
   let translatePipe: TranslatePipe;
+  let changeDetectorRef: FakeChangeDetectorRef;
+  let translateService: FakeTranslateService;
 
   beforeEach(() => {
-    const changeDetectorRef: Pick<ChangeDetectorRef, 'markForCheck'> = {
-      markForCheck(): void {},
-    };
-    const translateService = new FakeTranslateService();
+    changeDetectorRef = new FakeChangeDetectorRef();
+    translateService = new FakeTranslateService();
+
+    spyOn(changeDetectorRef, 'markForCheck');
+
     translatePipe = new TranslatePipe(
       changeDetectorRef as ChangeDetectorRef,
       translateService as TranslateService
     );
   });
 
-  xit('translates the key', () => {});
-  xit('translates a changed key', () => {});
-  xit('updates on translation change', () => {});
+  it('translates the key, sync service responsex', () => {
+    const translation = translatePipe.transform(key1);
+    expect(translation).toBe('[Translation for key1]');
+    expect(changeDetectorRef.markForCheck).toHaveBeenCalled();
+  });
+
+  it('translates the key, async service response (1)', () => {
+    const scheduler = new VirtualTimeScheduler();
+    translateService.get = (key: string) =>
+      of(`[Translation for ${key}]`).pipe(delay(1, scheduler));
+
+    const translation1 = translatePipe.transform(key1);
+    expect(translation1).toBe(null);
+    scheduler.flush();
+
+    const translation2 = translatePipe.transform(key1);
+    expect(translation2).toBe('[Translation for key1]');
+  });
+
+  it('translates the key, async service response (2)', fakeAsync(() => {
+    translateService.get = (key: string) =>
+      new Observable((subscriber) => {
+        setTimeout(() => {
+          subscriber.next(`[Translation for ${key}]`);
+          subscriber.complete();
+        }, 1);
+      });
+
+    const translation1 = translatePipe.transform(key1);
+    expect(translation1).toBe(null);
+    tick(1);
+
+    const translation2 = translatePipe.transform(key1);
+    expect(translation2).toBe('[Translation for key1]');
+  }));
+
+  it('gets a translation for a key only once', () => {
+    spyOn(translateService, 'get').and.callThrough();
+
+    const translation1 = translatePipe.transform(key1);
+    expect(translation1).toBe('[Translation for key1]');
+    expect(translateService.get).toHaveBeenCalledTimes(1);
+
+    const translation2 = translatePipe.transform(key1);
+    expect(translation2).toBe('[Translation for key1]');
+    expect(translateService.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('translates a changed key, sync response', () => {
+    const translation1 = translatePipe.transform(key1);
+    expect(translation1).toBe('[Translation for key1]');
+
+    const translation2 = translatePipe.transform(key2);
+    expect(translation2).toBe('[Translation for key2]');
+  });
+
+  it('updates on translation change', () => {
+    const translation1 = translatePipe.transform(key1);
+    expect(translation1).toBe('[Translation for key1]');
+    expect(changeDetectorRef.markForCheck).toHaveBeenCalledTimes(1);
+
+    translateService.get = (key: string) => of(`[New translation for ${key}]`);
+    translateService.onTranslationChange.emit({});
+
+    const translation2 = translatePipe.transform(key1);
+    expect(translation2).toBe('[New translation for key1]');
+    expect(changeDetectorRef.markForCheck).toHaveBeenCalledTimes(2);
+  });
 });
